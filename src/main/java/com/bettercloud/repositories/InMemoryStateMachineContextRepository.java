7b7d980249d096688ecab332237033349186a8cb
David Esposito <david.esposito@bettercloud.com>
2017-02-10 19:23:55 -0500

    Converting persistence layer to use json blobs for reloads.
    Signed-off-by: David Esposito <david.esposito@bettercloud.com>

--- src/main/java/com/bettercloud/repositories/InMemoryStateMachineContextRepository.java
+++ src/main/java/com/bettercloud/repositories/InMemoryStateMachineContextRepository.java
@@ -1,10 +1,18 @@
 package com.bettercloud.repositories;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.MissingNode;
 import com.google.common.collect.Maps;
+import org.springframework.statemachine.ExtendedState;
 import org.springframework.statemachine.StateMachineContext;
+import org.springframework.statemachine.support.DefaultExtendedState;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 /**
@@ -13,16 +21,45 @@ import java.util.UUID;
 @Service
 public class InMemoryStateMachineContextRepository implements StateMachineContextRepository {
 
-    private final Map<UUID, StateMachineContext<String, String>> contextMap = Maps.newConcurrentMap();
+    public static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    private final Map<UUID, String> contextMap = Maps.newConcurrentMap();
 
     @Override
     public void write(StateMachineContext<String, String> context, UUID contextObj) throws Exception {
-        contextMap.put(contextObj, context);
+        String contextJson = OBJECT_MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(context);
+        contextMap.put(contextObj, contextJson);
     }
 
     @Override
     public StateMachineContext<String, String> read(UUID contextObj) throws Exception {
-        return contextMap.get(contextObj);
+        return Optional.ofNullable(contextMap.get(contextObj))
+                .map(jsonContext -> convert(jsonContext))
+                .orElse(null);
+    }
+
+    private StateMachineContext<String, String> convert(String json) {
+        try {
+            JsonNode node = OBJECT_MAPPER.readTree(json);
+            String id = getString(node, "id");
+            String state = getString(node, "state");
+            String event = getString(node, "event");
+            Map<String, Object> eventHeaders = OBJECT_MAPPER.convertValue(node.path("eventHeaders"), Map.class);
+            Map<String, String> historyStates = OBJECT_MAPPER.convertValue(node.path("historyStates"), Map.class);
+            Map<Object, Object> stateVars = OBJECT_MAPPER.convertValue(node.path("extendedState").path("variables"), Map.class);
+            ExtendedState extendedState = new DefaultExtendedState(stateVars);
+            return new DefaultStateMachineContext<>(state, event, eventHeaders, extendedState, historyStates, id);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private String getString(JsonNode node, String key) {
+        return Optional.ofNullable(node.get(key))
+                .filter(JsonNode::isTextual)
+                .map(JsonNode::asText)
+                .orElse(null);
     }
 
     @Override
